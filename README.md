0ab1aa8cc7f470260c4eff5cdf933d52b19443b3
Sathabhron <thahi5_vi@hotmail.com>
2021-05-18 22:19:06 +0700

    Cats controller from office docs.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # start-nestjs
+
+yarn
+yarn run start
+nest g controller cats
+nest g service cats
+
+yarn add @nestjs/typeorm typeorm pg
+yarn add dotenv
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middileware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      //.forRoutes(CatsController);
+  }
+}
